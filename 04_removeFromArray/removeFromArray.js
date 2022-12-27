8062e48797d6467f8e159cd1fcd3568f09eb51b9
Spaceflyy <115657060+Spaceflyy@users.noreply.github.com>
2022-12-27 16:36:53 +0000

    Complete removeFromArray.js exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(workingArray, ...toRemove) {
+    let loopArray = workingArray;
+
+for (const args of toRemove)
+{
+    for(let i = 0; i < workingArray.length; i++)
+    {
+        if (workingArray[i] === args)
+        {
+            loopArray.splice(i,1);
+        }
+    }
+}
+    return loopArray;
 
 };
 
